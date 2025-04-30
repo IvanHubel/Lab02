7f8fa931e255e700c66f6991f7adfb08bf98dc4b
IvanHubel <ivan.tarazona@unmsm.edu.pe>
2025-04-30 11:25:00 -0500

    Update OrdenacionParalela.java

--- src/OrdenacionParalela.java
+++ src/OrdenacionParalela.java
@@ -1,15 +1,5 @@
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 import java.util.concurrent.RecursiveAction;
-
-/**
- *
- * @author Usuario
- */
 public class OrdenacionParalela extends RecursiveAction {
     private int[] arreglo;
     private int low;
